b1f25e7db22395942a1295fe70ec955902592485
Vitalii Prots <v.prots@yaware.com>
2017-11-30 17:25:52 +0200

    add new files

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
-var width = window.innerWidth;
-var height = window.innerHeight;
+var width = 800;
+var height = 500;
 
 var stage = new Konva.Stage({
   container: 'container',
@@ -12,8 +12,8 @@ var layer = new Konva.Layer();
 var rect = new Konva.Rect({
   x: 0,
   y: 0,
-  width: 400,
-  height: 400,
+  width: 800,
+  height: 500,
   fill: '#fff',
   stroke: 'black',
   strokeWidth: 2
@@ -25,18 +25,18 @@ layer.add(rect);
 // add the layer to the stage
 stage.add(layer);
 
-
-// add image
+// button img
 var imageObj = new Image();
-imageObj.onload = function () {
+var addPicture = document.getElementById('addColobri');
 
+addPicture.addEventListener('click', function () {
   var yoda = new Konva.Image({
-    x: 500,
-    y: 50,
+    x: 60,
+    y: 30,
     image: imageObj,
     width: 150,
     height: 150,
-    draggable: true
+    draggable: 'true'
   });
 
   // add the shape to the layer
@@ -44,58 +44,63 @@ imageObj.onload = function () {
 
   // add the layer to the stage
   stage.add(layer);
-};
+});
 imageObj.src = 'images/image.png';
 
+// button text
+var addText = document.getElementById("addText");
 
-// some text
+addText.addEventListener('click', function () {
+  var textNode = new Konva.Text({
+    text: 'Some text here',
+    x: 50,
+    y: 200,
+    fontSize: 24,
+    draggable: true,
+    fill: '#835773'
+  });
 
-var textNode = new Konva.Text({
-  text: 'Some text here',
-  x: 500,
-  y: 10,
-  fontSize: 24,
-  draggable: true,
-  fill: '#835773'
-});
+  layer.add(textNode);
+  layer.draw();
 
-layer.add(textNode);
-layer.draw();
+  textNode.on('dblclick', () => {
+    // create textarea over canvas with absolute position
 
-// textarea
+    // first we need to find its positon
+    var textPosition = textNode.getAbsolutePosition();
+    var stageBox = stage.getContainer().getBoundingClientRect();
 
-textNode.on('dblclick', () => {
-  // create textarea over canvas with absolute position
-
-  // first we need to find its positon
-  var textPosition = textNode.getAbsolutePosition();
-  var stageBox = stage.getContainer().getBoundingClientRect();
-
-  var areaPosition = {
-    x: textPosition.x + stageBox.left,
-    y: textPosition.y + stageBox.top
-  };
-
+    var areaPosition = {
+      x: textPosition.x + stageBox.left,
+      y: textPosition.y + stageBox.top
+    };
 
 // create textarea and style it
-  var textarea = document.createElement('textarea');
-  document.body.appendChild(textarea);
+    var textarea = document.createElement('textarea');
+    document.body.appendChild(textarea);
 
-  textarea.value = textNode.text();
-  textarea.style.position = 'absolute';
-  textarea.style.top = areaPosition.y + 'px';
-  textarea.style.left = areaPosition.x + 'px';
-  textarea.style.width = textNode.width();
+    textarea.value = textNode.text();
+    textarea.style.position = 'absolute';
+    textarea.style.top = areaPosition.y + 'px';
+    textarea.style.left = areaPosition.x + 'px';
+    textarea.style.width = textNode.width();
 
-  textarea.focus();
+    textarea.focus();
 
-
-  textarea.addEventListener('keydown', function (e) {
-    // hide on enter
-    if (e.keyCode === 13) {
-      textNode.text(textarea.value);
-      layer.draw();
-      document.body.removeChild(textarea);
-    }
+    textarea.addEventListener('keydown', function (e) {
+      // hide on enter
+      if (e.keyCode === 13) {
+        textNode.text(textarea.value);
+        layer.draw();
+        document.body.removeChild(textarea);
+      }
+    });
   });
+});
+
+
+var downloadImg = document.getElementById("download");
+
+downloadImg.addEventListener('click', function () {
+  
 });
